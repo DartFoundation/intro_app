69b3d8cb549577eef394c86137233a579002e5d1
ZARIFMAC <uzbzarif@gmail.com>
2023-08-17 10:20:09 +0500

    button

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  TextStyle textStyle = TextStyle(
+    fontSize: 30,
+    color: Colors.white,
+    decoration: TextDecoration.none,
+  );
   runApp(MaterialApp(
     home: Container(
         color: Colors.blueAccent,
         child: Column(
           children: [
             Container(
+              margin: EdgeInsets.all(20),
+              alignment: Alignment.center,
               color: Colors.redAccent,
-              width: 100,
+              width: 300,
               height: 100,
+              child: Text("Hello World", style: textStyle),
             ),
             Container(
               color: Colors.greenAccent,
