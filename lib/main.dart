06b9dea229a8d333418f42b2a6d75db6128f4e57
ZARIFMAC <uzbzarif@gmail.com>
2023-08-17 10:07:56 +0500

    column;

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,20 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Container(
-      child: Text('Hello World!'),
-    ),
+        color: Colors.blueAccent,
+        child: Column(
+          children: [
+            Container(
+              color: Colors.redAccent,
+              width: 100,
+              height: 100,
+            ),
+            Container(
+              color: Colors.greenAccent,
+              width: 100,
+              height: 100,
+            ),
+          ],
+        )),
   ));
 }
